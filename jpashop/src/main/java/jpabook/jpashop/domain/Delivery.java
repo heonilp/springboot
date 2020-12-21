e4af21661e25c5e14e2736eb498573b8a7a4f21d
heonilp <heonil8@nate.com>
2020-12-21 16:09:22 +0900

    spring boot jpa project
    엔티티 개발 강의 /코딩

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,2 +1,24 @@
-package jpabook.jpashop.domain;public class Delivery {
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //기본이 ORDINAL -> 숫자 중간에 다른 상태가 생기면 힘듬
+    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //strategy전략 JOINED , SINGLE_TABLE 한테이블(싱글테이블 전략)
+@DiscriminatorColumn(name = "dtype") //타입
 @Getter @Setter
 //추상 클래스 구현체를 들구해야함
 public abstract class Item {
