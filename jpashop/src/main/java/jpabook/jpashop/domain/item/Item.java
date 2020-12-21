81a3eee48fe4d547d1bb5fc5126f459c876ff5b5
heonilp <heonil8@nate.com>
2020-12-21 16:08:57 +0900

    spring boot jpa project
    엔티티 클래스 개발 강의/ 코딩

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain.item;
+import lombok.Getter;
+import lombok.Setter;
+import jpabook.jpashop.domain.Category;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+//추상 클래스 구현체를 들구해야함
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+
+}
