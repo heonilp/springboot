4556477d5fea6f8c02532dc86517f8f139baafdb
heonilp <heonil8@nate.com>
2020-12-24 16:37:12 +0900

    spring boot 강의듣기
    주능 기능추가, 유닛테스트 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,4 +1,5 @@
 package jpabook.jpashop.domain.item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import jpabook.jpashop.domain.Category;
@@ -7,7 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //strategy전략 JOINED , SINGLE_TABLE 한테이블(싱글테이블 전략)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//strategy전략 JOINED , SINGLE_TABLE 한테이블(싱글테이블 전략)
 @DiscriminatorColumn(name = "dtype") //타입
 @Getter @Setter
 //추상 클래스 구현체를 들구해야함
@@ -22,5 +24,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    // 비지니스 로직 추가
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 
 }
